cadccc913a2e106a2e5b9a04f557b5e236ad239c
Mayte <mayte.valdez.abad@gmail.com>
2020-12-31 17:37:15 +0100

    add props

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import Navbar from './components/Navbar';
 import QuoteCard from './components/QuoteCard';
 
@@ -5,7 +6,21 @@ function App() {
   return (
     <div className="App">
       <Navbar/>
-      <QuoteCard/>
+      <QuoteCard 
+        character="Nelson Muntz" 
+        quote='Shoplifting is a victimless crime, like punching someone in the dark.'
+        image="https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FNelsonMuntz.png?1497567511185"
+      />
+      <QuoteCard 
+        character="Ralph Wiggum" 
+        quote="Me fail English? That's unpossible."
+        image="https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FRalphWiggum.png?1497567511523"
+      />
+      <QuoteCard 
+        character="Milhouse Van Houten" 
+        quote='Remember the time he ate my goldfish? And you lied and said I never had a goldfish. Then why did I have the bowl, Bart? Why did I have the bowl?'
+        image="https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FMilhouseVanHouten.png?1497567513002"
+      />
     </div>
   );
 }
